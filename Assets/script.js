f60a81c3e9d3a80bf1c34f89cd6f700dd0b8b75d
mdavie <marianahodniki1991@gmail.com>
2021-10-06 10:08:03 -0600

    added search button click event and storage

--- Assets/script.js
+++ Assets/script.js
@@ -12,3 +12,19 @@ for (var i = 0; i < localStorage.length; i++) {
 
     cityName.append("<li>" + city + "</li>");
 }
+
+// Key count for local storage 
+var keyCount = 0;
+
+
+// Search button click event
+searchButton.click(function () {
+
+    var searchInput = $(".searchInput").val();
+
+    // Variable for current weather working 
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    // Variable for 5 day forecast working
+    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+
+ 
