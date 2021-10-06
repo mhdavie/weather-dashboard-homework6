1caf065b1bf3620cc09ced23ff042fcf56d95d6a
mdavie <marianahodniki1991@gmail.com>
2021-10-06 11:37:28 -0600

    bootraps containers and cards added and css personal styles added

--- Assets/script.js
+++ Assets/script.js
@@ -64,3 +64,44 @@ searchButton.click(function () {
   // // Add Wind Speed: 
   currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
 
+
+// UV Index URL
+              var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+
+              // UV Index
+              $.ajax({
+                  url: urlUV,
+                  method: "GET"
+              }).then(function (response) {
+  
+                  var currentUV = currentTemp.append("<p>" + "UV Index: " + response.value + "</p>").addClass("card-text");
+                  currentUV.addClass("UV");
+                  currentTemp.append(currentUV);
+                  // currentUV.append("UV Index: " + response.value);
+              });
+  
+          });
+  
+          // Start call for 5-day forecast 
+          $.ajax({
+              url: urlFiveDay,
+              method: "GET"
+          }).then(function (response) {
+              // Array for 5-days 
+              var day = [0, 8, 16, 24, 32];
+              var fiveDayCard = $(".fiveDayCard").addClass("card-body");
+              var fiveDayDiv = $(".fiveDayOne").addClass("card-text");
+              fiveDayDiv.empty();
+              // For each for 5 days
+              day.forEach(function (i) {
+                  var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
+                  FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-US");
+  
+                  fiveDayDiv.append("<div class=fiveDayColor>" + "<p>" + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
+  
+  
+              })
+  
+          });
+      }
+  });
