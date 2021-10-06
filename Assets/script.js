a75d5caebac7cd654d85e849b0fddb9efb185f32
mdavie <marianahodniki1991@gmail.com>
2021-10-05 21:51:53 -0600

    set variables

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+// Variables 
+var searchButton = $(".searchButton");
+
+var apiKey = "a2be8081e76d81dc48560ed9a9eaec38";
+
+// Forloop for persisting the data onto HMTL page
+for (var i = 0; i < localStorage.length; i++) {
+
+    var city = localStorage.getItem(i);
+    // console.log(localStorage.getItem("City"));
+    var cityName = $(".list-group").addClass("list-group-item");
+
+    cityName.append("<li>" + city + "</li>");
+}
